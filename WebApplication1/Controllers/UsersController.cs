e823657492370eaf169d2e7dc680d1662130af02
AdamMu108 <s12113269@stu.najah.edu>
2026-05-11 13:58:28 +0300

    crud task1

---
+++ WebApplication1/Controllers/UsersController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Data;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class UsersController : Controller
+    {
+        ApplicationDbContext context = new ApplicationDbContext();
+
+        public ViewResult Index()
+        {
+            var users = context.Users.ToList();
+            return View("Index", users);
+        }
+
+        public ViewResult Create()
+        {
+            return View("Create");
+        }
+        public ViewResult Store(User request)
+        {
+            context.Users.Add(request);
+            context.SaveChanges();
+            return View("Create");
+        }
+        public string All()
+        {
+            return "All users";
+        }
+    }
+}
---
+++ WebApplication1/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+namespace WebApplication1.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Data Source=.;Database=MVC1;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False");
+        }
+    }
+}
