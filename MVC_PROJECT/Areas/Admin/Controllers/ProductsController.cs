f741dc7729f45654417c1bd37738f2efe8b08e0e
AdamMu108 <s12113269@stu.najah.edu>
2026-06-10 16:30:09 +0300

    all CRUD for categories and products

--- MVC_PROJECT/Areas/Admin/Controllers/ProductsController.cs
+++ MVC_PROJECT/Areas/Admin/Controllers/ProductsController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MVC_PROJECT.Data;
+using MVC_PROJECT.Models;
 
 namespace MVC_PROJECT.Areas.Admin.Controllers
 {
@@ -10,8 +11,44 @@ namespace MVC_PROJECT.Areas.Admin.Controllers
         ApplicationDbContext context = new ApplicationDbContext();
         public IActionResult Index()
         {
-            var products = context.Products.Include(p => p.Category).ToList();
+            var products = context.Products.ToList();
+            ViewBag.categories = context.Categories.ToList();
             return View(products);
+        }
+        public IActionResult Create()
+        {
+            ViewBag.categories = context.Categories.ToList();
+            return View();
+        }
+        public IActionResult Store(Product request)
+        {
+            context.Products.Add(request);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public IActionResult Delete(int id)
+        {
+            var product = context.Products.Find(id);
+            if (product != null)
+            {
+                context.Products.Remove(product);
+                context.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+        public IActionResult Edit(int id)
+        {
+            var product = context.Products.Find(id);
+            ViewBag.categories = context.Categories.ToList();
+            return View(product);
+        }
+        public IActionResult Update(Product request, int id)
+        {
+            request.Id = id;
+
+            context.Products.Update(request);
+            context.SaveChanges();
+            return RedirectToAction("Index");
         }
 
     }
--- MVC_PROJECT/obj/Debug/net9.0/MVC_PROJECT.AssemblyInfo.cs
+++ MVC_PROJECT/obj/Debug/net9.0/MVC_PROJECT.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MVC_PROJECT")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+064b73a80cb1f19333868da7426c5f923ba148e2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7354cd4944b374abec641229b64e57e30113c75f")]
 [assembly: System.Reflection.AssemblyProductAttribute("MVC_PROJECT")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MVC_PROJECT")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
